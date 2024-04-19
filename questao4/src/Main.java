0033b332bea77313cb6c2827577e63545dbb19ad
Alexandre Gonçalves Evangelista <140849154+Alexandre-Evangelista@users.noreply.github.com>
2024-04-19 14:33:58 -0300

    corrigindo erro questao4

--- questao4/src/Main.java
+++ questao4/src/Main.java
@@ -2,17 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double i;double soma=0;int cont=0;
+        double i;double soma=0;double cont=0;
         Scanner entrada= new Scanner(System.in);
         i= entrada.nextDouble();
+        soma+=i;
         while (i>=0){
             i= entrada.nextDouble();
-            soma+=i;
+            if (i>=0){
+                soma+=i;
+            }
             cont++;
         }
         System.out.println("a media e de "+soma/cont);
         System.out.println("a quantidade e de "+cont);
-
 
     entrada.close();
     }
